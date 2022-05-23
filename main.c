dee463ccc104e9d8a1bfd58a93968e38a90d879b
awei <yxw_fighting@126.com>
2022-05-23 21:33:56 +0800

    hello github

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+
+int main(int argc, char *argv[])
+{
+	char buf[280]={0};
+
+	printf("\n hahaha Hello Git\n");
+	
+	getchar();
+	return 0;
+}
